327b3fad08e16f21bb1d2644edbcc0c68bb39057
Джеффри <jeffjeffreytagne@gmail.com>
2025-03-29 10:45:45 +0100

    New message

--- main.py
+++ main.py
@@ -20,9 +20,8 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 
 async def approve_request(chat_join: ChatJoinRequest, bot: Bot):
-    msg = ("Olá beleza 👋🏻👸🏻 Estou aqui para te mostrar uma maneira legal de ganhar comigo.\n"
-           "\nSou o bot da Samira e posso te enviar as melhores ofertas se você me escrever agora mesmo 🤖\n"
-           "\nMe escreve 👉🏻@Samira_Rod_vip")
+    msg = ("Bem vindo ao canal de Isabela Santana 📈 Sou o bot 🤖 de Isabela\n"
+           "Mande uma mensagem agora para obter todas orientações @SofiadelasT")
     await bot.send_message(chat_id=chat_join.from_user.id, text=msg)
     await chat_join.approve()
 
